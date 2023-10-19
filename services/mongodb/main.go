9d56f93639907fe646975e73e24dc090f1e059ec
cemye <cem.yesil@me.com>
2023-10-19 11:25:38 +0200

    add main, move stuff around

--- services/mongodb/main.go
+++ services/mongodb/main.go
@@ -3,92 +3,116 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"flag"
+	"github.com/golang/glog"
 	"github.com/hobbyfarm/gargantua/pkg/util"
 	"net/http"
+	"os"
 	"strconv"
 	"time"
 
-	"github.com/golang/glog"
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type Config struct {
-	URI            string
-	dbName         string
-	collectionName string
-	servicePort    int
-	username       string
-	password       string
-}
-
-type Server struct {
-	config     Config
-	collection *mongo.Collection
-}
-
-func NewServer(config Config) *Server {
-	return &Server{config: config}
-}
-
-func (s *Server) initMongoDB() error {
-	credentials := options.Credential{
-		AuthSource:    "admin",
-		AuthMechanism: "SCRAM-SHA-256",
-		Username:      s.config.username,
-		Password:      s.config.password,
-	}
-
-	clientOptions := options.Client().ApplyURI(s.config.URI).SetAuth(credentials)
-	client, err := mongo.Connect(context.Background(), clientOptions)
-	if err != nil {
-		return err
-	}
-	s.collection = client.Database(s.config.dbName).Collection(s.config.collectionName)
-	return nil
-}
-
-func (s *Server) start() error {
-	r := mux.NewRouter()
-	s.setupRoutes(r)
-
-	// Start the server
-	srv := &http.Server{
-		Addr:         ":" + strconv.Itoa(s.config.servicePort),
-		Handler:      r,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 10 * time.Second,
-	}
-
-	glog.Info("Starting server on port ", s.config.servicePort)
-
-	if err := srv.ListenAndServe(); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (s *Server) setupRoutes(r *mux.Router) {
-	r.HandleFunc("/api/items", s.getItems).Methods("GET")
-	r.HandleFunc("/api/items/{id}", s.getItem).Methods("GET")
-	r.HandleFunc("/api/items", s.createItem).Methods("POST")
-	r.HandleFunc("/api/items/{id}", s.updateItem).Methods("PUT")
-	r.HandleFunc("/api/items/{id}", s.deleteItem).Methods("DELETE")
-	r.HandleFunc("/api/items/{id}", s.appendDataToItem).Methods("PATCH")
-	glog.V(2).Infof("API Routes are set up")
-}
-
 type Item struct {
 	Id   string                 `bson:"_id" json:"id"`
 	Data map[string]interface{} `bson:"data" json:"data"`
 }
 
-func (s *Server) getItems(w http.ResponseWriter, r *http.Request) {
+var (
+	collection *mongo.Collection
+
+	URI            string
+	dbName         string
+	collectionName string
+	servicePort    int
+
+	username string
+	password string
+)
+
+func init() {
+	flag.StringVar(&URI, "mongoURI", "mongodb://mongo-db-service.hobbyfarm.svc.cluster.local:27017", "URI of the mongodb")
+	flag.StringVar(&dbName, "dbName", "hobbyfarmDB", "Name of the mongodb")
+	flag.StringVar(&collectionName, "collection", "sessionData", "Name of the collection in the database")
+	flag.IntVar(&servicePort, "servicePort", 8080, "Port to run service on")
+
+	flag.StringVar(&username, "username", "", "Username for MongoDB authentication.")
+	flag.StringVar(&password, "password", "", "Password for MongoDB authentication.")
+
+	// Use env if available
+	if envURI := os.Getenv("DB_URI"); envURI != "" {
+		URI = envURI
+	}
+	if envName := os.Getenv("DB_NAME"); envName != "" {
+		dbName = envName
+	}
+	if envCollection := os.Getenv("DB_COLLECTION"); envCollection != "" {
+		collectionName = envCollection
+	}
+
+	if envUsername := os.Getenv("DB_USERNAME"); envUsername != "" {
+		username = envUsername
+	}
+	if envPassword := os.Getenv("DB_PASSWORD"); envPassword != "" {
+		password = envPassword
+	}
+}
+
+func main() {
+	flag.Parse()
+	r := mux.NewRouter()
+	SetupRoutes(r)
+
+	credentials := options.Credential{
+		AuthSource:    "admin",
+		AuthMechanism: "SCRAM-SHA-256",
+		Username:      username,
+		Password:      password,
+	}
+
+	// Create MongoDB client
+	clientOptions := options.Client().ApplyURI(URI).SetAuth(credentials)
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		glog.Fatal(err)
+	}
+	defer client.Disconnect(context.Background())
+
+	// Select MongoDB collection
+	collection = client.Database(dbName).Collection(collectionName)
+
+	// Start the server
+	srv := &http.Server{
+		Addr:         ":" + strconv.Itoa(servicePort),
+		Handler:      r,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	glog.Info("Starting server on port ", servicePort)
+
+	if err := srv.ListenAndServe(); err != nil {
+		glog.Fatal(err)
+	}
+}
+
+func SetupRoutes(r *mux.Router) {
+	r.HandleFunc("/api/items", getItems).Methods("GET")
+	r.HandleFunc("/api/items/{id}", getItem).Methods("GET")
+	r.HandleFunc("/api/items", createItem).Methods("POST")
+	r.HandleFunc("/api/items/{id}", updateItem).Methods("PUT")
+	r.HandleFunc("/api/items/{id}", deleteItem).Methods("DELETE")
+	r.HandleFunc("/api/items/{id}", appendDataToItem).Methods("PATCH")
+	glog.V(2).Infof("API Routes are set up")
+}
+
+func getItems(w http.ResponseWriter, r *http.Request) {
 	glog.Info("Received getItems")
 	// Fetch all items from the MongoDB collection
-	cur, err := s.collection.Find(context.Background(), bson.D{})
+	cur, err := collection.Find(context.Background(), bson.D{})
 	if err != nil {
 		util.ReturnHTTPErrorMessage(w, r, http.StatusInternalServerError, err.Error())
 		return
@@ -113,14 +137,14 @@ func (s *Server) getItems(w http.ResponseWriter, r *http.Request) {
 	returnJSONResponse(w, r, http.StatusOK, items)
 }
 
-func (s *Server) getItem(w http.ResponseWriter, r *http.Request) {
+func getItem(w http.ResponseWriter, r *http.Request) {
 	glog.Info("Received getItem")
 	// Extract the item ID from the URL path parameter
 	id := mux.Vars(r)["id"]
 
 	// Find the item with the given ID in the MongoDB collection
 	filter := bson.M{"_id": id}
-	result := s.collection.FindOne(context.Background(), filter)
+	result := collection.FindOne(context.Background(), filter)
 
 	var item Item
 	if err := result.Decode(&item); err != nil {
@@ -131,17 +155,18 @@ func (s *Server) getItem(w http.ResponseWriter, r *http.Request) {
 	returnJSONResponse(w, r, http.StatusOK, item)
 }
 
-func (s *Server) createItem(w http.ResponseWriter, r *http.Request) {
+func createItem(w http.ResponseWriter, r *http.Request) {
 	glog.Info("Received createItem")
 
 	// Parse the request body into an Item struct
 	var item Item
-	if err := decodeJSONRequestAndHandleError(w, r, &item); err != nil {
+	if err := decodeJSONRequest(r, &item); err != nil {
+		util.ReturnHTTPErrorMessage(w, r, http.StatusBadRequest, "Invalid request payload")
 		return
 	}
 
 	// Insert the item into the MongoDB collection
-	if _, err := s.collection.InsertOne(context.Background(), item); err != nil {
+	if _, err := collection.InsertOne(context.Background(), item); err != nil {
 		util.ReturnHTTPErrorMessage(w, r, http.StatusInternalServerError, err.Error())
 		return
 	}
@@ -149,23 +174,24 @@ func (s *Server) createItem(w http.ResponseWriter, r *http.Request) {
 	returnJSONResponse(w, r, http.StatusOK, item)
 }
 
-func (s *Server) updateItem(w http.ResponseWriter, r *http.Request) {
+func updateItem(w http.ResponseWriter, r *http.Request) {
 	id := mux.Vars(r)["id"]
+	// Parse the request body into an Item struct
 	var item Item
-
-	if err := decodeJSONRequestAndHandleError(w, r, &item); err != nil {
+	if err := decodeJSONRequest(r, &item); err != nil {
+		util.ReturnHTTPErrorMessage(w, r, http.StatusBadRequest, "Invalid request payload")
 		return
 	}
 
 	// Update the item in the MongoDB collection
 	filter := bson.M{"_id": id}
 	update := bson.M{"$set": bson.M{"data": item.Data}}
-	result, err := s.collection.UpdateOne(context.Background(), filter, update)
-
+	result, err := collection.UpdateOne(context.Background(), filter, update)
 	if err != nil {
 		util.ReturnHTTPErrorMessage(w, r, http.StatusInternalServerError, err.Error())
 		return
 	}
+
 	if result.MatchedCount == 0 {
 		util.ReturnHTTPErrorMessage(w, r, http.StatusNotFound, "Item not found")
 		return
@@ -174,18 +200,23 @@ func (s *Server) updateItem(w http.ResponseWriter, r *http.Request) {
 	returnJSONResponse(w, r, http.StatusOK, item)
 }
 
-func (s *Server) appendDataToItem(w http.ResponseWriter, r *http.Request) {
+func appendDataToItem(w http.ResponseWriter, r *http.Request) {
+	// Extract the item ID from the URL path parameter
 	id := mux.Vars(r)["id"]
-	var item map[string]interface{}
 
-	if err := decodeJSONRequestAndHandleError(w, r, &item); err != nil {
+	// Parse the request body into a map[string]interface{} to get the data to append
+	var requestData map[string]interface{}
+	if err := decodeJSONRequest(r, &requestData); err != nil {
+		util.ReturnHTTPErrorMessage(w, r, http.StatusBadRequest, "Invalid request payload")
 		return
 	}
 
-	filter := bson.M{"_id": id}
-	update := bson.M{"$addToSet": bson.M{"data": item}}
-	result, err := s.collection.UpdateOne(context.Background(), filter, update)
+	// Create an update query to append data to the existing item
+	update := bson.M{"$addToSet": bson.M{"data": requestData}}
 
+	// Find and update the item in the MongoDB collection
+	filter := bson.M{"_id": id}
+	result, err := collection.UpdateOne(context.Background(), filter, update)
 	if err != nil {
 		util.ReturnHTTPErrorMessage(w, r, http.StatusInternalServerError, err.Error())
 		return
@@ -196,16 +227,16 @@ func (s *Server) appendDataToItem(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	returnJSONResponse(w, r, http.StatusOK, item)
+	returnJSONResponse(w, r, http.StatusOK, requestData)
 }
 
-func (s *Server) deleteItem(w http.ResponseWriter, r *http.Request) {
+func deleteItem(w http.ResponseWriter, r *http.Request) {
 	// Extract the item ID from the URL path parameter
 	id := mux.Vars(r)["id"]
 
 	// Delete the item from the MongoDB collection
 	filter := bson.M{"_id": id}
-	result, err := s.collection.DeleteOne(context.Background(), filter)
+	result, err := collection.DeleteOne(context.Background(), filter)
 	if err != nil {
 		util.ReturnHTTPErrorMessage(w, r, http.StatusInternalServerError, err.Error())
 		return
@@ -216,6 +247,7 @@ func (s *Server) deleteItem(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//todo check messageType
 	util.ReturnHTTPMessage(w, r, http.StatusOK, "success", "Item deleted successfully")
 }
 
@@ -224,16 +256,7 @@ func decodeJSONRequest(r *http.Request, v interface{}) error {
 	return json.NewDecoder(r.Body).Decode(v)
 }
 
-func decodeJSONRequestAndHandleError(w http.ResponseWriter, r *http.Request, v interface{}) error {
-	if err := decodeJSONRequest(r, v); err != nil {
-		util.ReturnHTTPErrorMessage(w, r, http.StatusBadRequest, "Invalid request payload")
-		return err
-	}
-	return nil
-}
-
 // Return a JSON response
-
 func returnJSONResponse(w http.ResponseWriter, r *http.Request, status int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
